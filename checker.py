e00d676d6e54b0a30ab4a5177ef5292c2f9f43b2
Morgen Sudyanto <moondemon68@gmail.com>
2019-11-05 13:21:29 +0700

    duar fungsi

--- checker.py
+++ checker.py
@@ -2,44 +2,55 @@ import os
 from extract import *
 from compare import *
 
-feature_file = 'features.dmp'
-name_file = 'names.dmp'
-
 features = []
-f = open(feature_file, 'r')
-for line in f.readlines():
-    if (line[-1:] == '\n'):
-        line = line[:-1]
-    features.append(line.split(' '))
-
 names = []
-g = open(name_file, 'r')
-for name in g.readlines():
-    name = name[:-1]
-    names.append(name)
 
-img_file = '2.jpg'
-img = extract_features(img_file)
+def load():
+    feature_file = 'features.dmp'
+    name_file = 'names.dmp'
 
-p = int(input())
+    f = open(feature_file, 'r')
+    for line in f.readlines():
+        if (line[-1:] == '\n'):
+            line = line[:-1]
+        features.append(line.split(' '))
 
-if (p == 0):
-    result = []
-    for i in range(len(features)):
-        x = dist(img, features[i])
-        result.append((x, i))
-    result.sort()
+    g = open(name_file, 'r')
+    for name in g.readlines():
+        name = name[:-1]
+        names.append(name)
 
-    for i in range(5):
-        print(result[i][1])
-        print(names[result[i][1]])
-elif (p == 1):
-    result = []
-    for i in range(len(features)):
-        x = cosine(img, features[i])
-        result.append((x, i))
-    result.sort()
+def compareImage(mode, n):
+    img_file = 'cmp.jpg'
+    img = extract_features(img_file)
 
-    for i in range(5):
-        print(result[i][1])
-        print(names[result[i][1]])
+    if (mode == 0):
+        result = []
+        for i in range(len(features)):
+            x = dist(img, features[i])
+            result.append((x, i))
+        result.sort()
+        
+        result = result[:n]
+        for i in range(len(result)):
+            result[i] = names[result[i][1]]
+        return result
+
+    elif (mode == 1):
+        result = []
+        for i in range(len(features)):
+            x = cosine(img, features[i])
+            result.append((x, i))
+        result.sort()
+        result = result[::-1]
+
+        result = result[:n]
+        for i in range(len(result)):
+            result[i] = names[result[i][1]]
+        return result
+
+load()
+ans = compareImage(1, 4)
+print(ans)
+ans = compareImage(0, 4)
+print(ans)
