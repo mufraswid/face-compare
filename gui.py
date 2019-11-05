bcb8d434a51682d48ef41d92f007165d6e0fc237
Ramunechoco <48040337+ramunechoco@users.noreply.github.com>
2019-11-05 19:22:40 +0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -6,37 +6,41 @@ from checker import *
 
 class ViewerPanel(wx.Panel):
     """"""
-    def OnRadiogroup(self, e): 
-        rb = e.GetEventObject() 
-        print rb.GetLabel(),' is clicked from Radio Group' 
-            
-    def onRadioBox(self,e): 
-        print self.rbox.GetStringSelection(),' is clicked from Radio Box'
     #----------------------------------------------------------------------
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
         load()
         width, height = wx.DisplaySize()        
-        self.photoMaxSize = height - 200
+        self.photoMaxSize = 290
         self.layout()
     #----------------------------------------------------------------------
     def layout(self):
-        pnl = wx.Panel(self)
         self.mainSizer = wx.BoxSizer(wx.VERTICAL)
-        btnSizer = wx.BoxSizer(wx.HORIZONTAL)        
+        sizer = wx.BoxSizer(wx.HORIZONTAL)        
         img = wx.EmptyImage(self.photoMaxSize,self.photoMaxSize)
-        load()
-
         self.imageCtrl = wx.StaticBitmap(self, wx.ID_ANY, 
-                                         wx.BitmapFromImage(img))
+                                         wx.Bitmap(img))
+        self.imageCtrl2 = wx.StaticBitmap(self, wx.ID_ANY, 
+                                         wx.Bitmap(img))
         uplBtn = wx.Button(self, label='Upload a Picture')
         uplBtn.Bind(wx.EVT_BUTTON,self.upload)
-        address = self.upload
         runBtn = wx.Button(self, label='Run the Program')
-        runBtn.Bind(wx.EVT_BUTTON,self.programRun)
-        btnSizer.Add(uplBtn, 0, wx.ALL|wx.CENTER, 5)
-
-        self.mainSizer.Add(btnSizer, 0, wx.CENTER)           
+        runBtn.Bind(wx.EVT_BUTTON,self.runProgram)        
+        sizer.Add(self.imageCtrl, 0, wx.ALL, 5)
+        sizer.Add(self.imageCtrl2, 0, wx.ALL, 5)      
+        self.mainSizer.Add(sizer, 0, wx.CENTER)
+        self.mainSizer.Add(uplBtn, 0, wx.ALL|wx.CENTER, 5)
+        self.mainSizer.Add(runBtn, 0, wx.ALL|wx.CENTER, 5)
+        self.inputMode = wx.Choice(self, choices = ['Distribution', 'Cosine'])
+        self.inputInt = wx.Slider(self, 10, 1, 1, 10, style=wx.SL_HORIZONTAL|wx.SL_AUTOTICKS|wx.SL_LABELS)
+        self.n = self.inputInt.GetValue()        
+        self.inputRank = wx.SpinButton(self, style=wx.SP_VERTICAL)        
+        self.inputRank.SetRange(1, self.n)
+        self.inputRank.SetValue(1)
+        self.inputRank.Bind(wx.EVT_BUTTON,self.display)
+        self.mainSizer.Add(self.inputMode, 0, wx.ALL, 5)        
+        self.mainSizer.Add(self.inputRank, 0, wx.ALL, 5)
+        self.mainSizer.Add(self.inputInt, 0, wx.ALL, 5)
         self.SetSizer(self.mainSizer)
     #----------------------------------------------------------------------
     def upload(self,event):
@@ -45,69 +49,43 @@ class ViewerPanel(wx.Panel):
                                wildcard=wildcard,
                                style=wx.FD_OPEN)
         if dialog.ShowModal() == wx.ID_OK:
-            address = dialog.GetPath()
-        return address
+            self.address = dialog.GetPath()
         dialog.Destroy()
-        self.OnView()
-
-    #----------------------------------------------------------------------
-    def onView(self):
-        filepath = self.photoTxt.GetValue()
-        img = wx.Image(filepath, wx.BITMAP_TYPE_ANY)
-        W = img.GetWidth()
-        H = img.GetHeight()
-        img = img.Scale(W,H)
-        self.imageCtrl.SetBitmap(wx.BitmapFromImage(img))
+        img = wx.Image(self.address, wx.BITMAP_TYPE_ANY)
+        self.imageCtrl.SetBitmap(wx.Bitmap(img))
         self.Refresh()
         self.mainSizer.Fit(self)
     #----------------------------------------------------------------------
-    def programRun(self):
-        dlgone = wx.TextEntryDialog(self, 'Insert number here', 'Images Amount',
-                                style=wx.OK)
-        dlgone.ShowModal()
-        n = dlgone.GetValue()
-        dlgone.Destroy()
-        dlgtwo = wx.Choice(self, choices = ['dist', 'cosine'])
-        mode = dlgtwo.GetString()
-        dlgtwo.Destroy()
-        compareImage(address, mode, n)
-        
+    def mode(self):
+        tipe = self.inputMode.GetSelection()
+        if (tipe == 'Distribution'):
+            return 0;
+        if (tipe == 'Cosine'):
+            return 1;
+    #----------------------------------------------------------------------
+    def runProgram(self,event):
+        arr = compareImage(self.address, self.mode, self.n)
+        self.inputRank.Enable()
+        return arr
+    #----------------------------------------------------------------------
+    def display(self,event):
+        i = self.inputRank.GetValue()
+        img = self.RunProgram()
+        self.imageCtrl2.SetBitmap(wx.Bitmap(img[i]))
+        self.Refresh()
+        self.mainsizer.Fit(self)
 #----------------------------------------------------------------------
 class ViewerFrame(wx.Frame):
     #----------------------------------------------------------------------
     def __init__(self):
         wx.Frame.__init__(self, None, title="FaceRecog")
         panel = ViewerPanel(self)
-        self.initToolbar()        
-        self.folderPath = ""
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.sizer.Add(panel, 1, wx.EXPAND)
         self.SetSizer(self.sizer)
         self.Show()
         self.sizer.Fit(self)
         self.Center()
-    #----------------------------------------------------------------------
-    def initToolbar(self):
-        self.toolbar = self.CreateToolBar()
-        self.toolbar.SetToolBitmapSize((16,16))
-        open_ico = wx.ArtProvider.GetBitmap(wx.ART_FILE_OPEN, wx.ART_TOOLBAR, (16,16))
-        openTool = self.toolbar.AddSimpleTool(wx.ID_ANY, open_ico, "Open", "Open an Image Directory")
-        self.Bind(wx.EVT_MENU, self.onOpenDirectory, openTool)
-        self.toolbar.Realize()
-        
-    #----------------------------------------------------------------------
-    def onOpenDirectory(self, event):
-        dlg = wx.FileDialog(frame, "Open", "", "", 
-      "JPEG files (*.jpg)|*.jpg", 
-       wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
-        
-        if dlg.ShowModal() == wx.ID_OK:
-            self.folderPath = dlg.GetPath()
-            print(self.folderPath)
-            self.photoTxt.SetValue(dialog.GetPath())
-        dialog.Destroy() 
-        self.onView()
-        
 #----------------------------------------------------------------------
 if __name__ == "__main__":
     app = wx.App()
